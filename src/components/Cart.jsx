f77c43776b552b1110a7dc29e4b6d29edc2a1252
Shadab khan <119776858+shadabBhai@users.noreply.github.com>
2024-03-18 07:19:45 +0530

    cart funtionality

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,13 +1,34 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import UserContext from "../utils/userContext";
 import CartList from "./CartList";
 
 const Cart = () => {
   const { cart } = useContext(UserContext);
+  const [totalAmount, setTotalAmount] = useState(0);
+
+  useEffect(() => {
+    const calculate = () => {
+      let total = 0;
+      cart.forEach((element) => {
+        total += element?.price || item?.defaultPrice || 0;
+      });
+      setTotalAmount(total / 100);
+    };
+    calculate();
+  }, [cart]);
 
   return (
     <div>
-      <div>Your cart : {cart} </div>
+      <div className="text-center text-lg font-bold">
+        Your cart ₹ {totalAmount}
+      </div>
+      <div>
+        <div>
+          {cart.map((item) => {
+            return <CartList key={item?.id} data={item} />;
+          })}
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/CartList.jsx
+++ src/components/CartList.jsx
@@ -1,8 +1,17 @@
+import { CDN_URL } from "../utils/constants";
+
 const CartList = ({ data }) => {
-  console.log(data);
   return (
-    <div>
-      <div></div>
+    <div className="p-4 m-4  border-b-2 flex justify-between">
+      <div className="">
+        <div className="font-bold text-lg">{data?.name}</div>
+        <div>
+          ₹ {data?.price ? data?.price / 100 : data?.defaultPrice / 100}
+        </div>
+      </div>
+      <div>
+        <img src={CDN_URL + data?.imageId} alt="" className="w-36" />
+      </div>
     </div>
   );
 };
