a4655aad272007e2faffe7844beb7c3eec58b86c
Shadab khan <119776858+shadabBhai@users.noreply.github.com>
2024-04-02 14:09:34 +0530

    testing on about page

--- src/components/__tests__/About.test.js
+++ src/components/__tests__/About.test.js
@@ -1,12 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import Contact from "../Contact";
 
-import "@testing-library/jest-dom/extend-expect";
+import "@testing-library/jest-dom";
 
 test("test for about section", () => {
   render(<Contact />);
 
   const heading = screen.getByRole("heading");
 
-  expect(heading).toBeInDocument();
+  expect(heading).toBeInTheDocument();
 });
--- src/components/__tests__/sum.test.js
+++
@@ -1,7 +0,0 @@
-import { sum } from "../sum";
-
-test("test the sum function", () => {
-  const testCase1 = sum(2, 3);
-
-  expect(testCase1).toBe(9);
-});
